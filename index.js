34a08359cc1b5167251c41f232d4ba7f5d3a1f4d
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-04 11:59:11 +0800

    Finish exercises 3.1-3.6

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+
+app.use(express.json());
 
 let persons = [
   {
@@ -24,32 +26,65 @@ let persons = [
   },
 ];
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end()
-    }
-})
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
 
-app.get('/info', (request, response) => {
-    const numOfPeople = persons.length
-    const time = new Date(new Date().getTime())
-    response.send(`<p>The phonebook has info for ${numOfPeople} people</p>
-    <p>${time}</p>`)
-})
+app.get("/info", (request, response) => {
+  const numOfPeople = persons.length;
+  const time = new Date(new Date().getTime());
+  response.send(`<p>The phonebook has info for ${numOfPeople} people</p>
+    <p>${time}</p>`);
+});
 
-const PORT = 3001
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+  console.log("deletion made");
+});
+
+app.post("/api/persons", (request, response) => {
+  const randomId = Math.floor(Math.random() * 1000);
+
+  const body = request.body;
+
+  if (!body.name && !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "Person already exists",
+    });
+  }
+  const person = {
+    id: randomId,
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
